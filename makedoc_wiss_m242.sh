faf331e87c5d2c58d3f1583238dcb28d0f51997f
greenorca <sven.glueckspilz@mastersong.de>
2017-06-20 22:16:25 +0200

    updated mainly wiss slides 3 (asm) and 4 (timer, interrupt)

--- makedoc_wiss_m242.sh
+++
@@ -1,2 +0,0 @@
-PATH=$HOME/.cabal/bin:$PATH
-pandoc -s README.md -o kurs_wiss_m242.html --toc --toc-depth 2 -N -c style_wiss.css --filter pandoc-include --section -t html5 --self-contained
--- makedoc_zhb.sh
+++
@@ -1,2 +0,0 @@
-PATH=$HOME/.cabal/bin:$PATH
-pandoc -s README.md -o kurs_zhb.html --toc --toc-depth 2 -N -c style_zhb.css --filter pandoc-include --section -t html5 --self-contained
